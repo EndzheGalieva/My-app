9ce42aab63d88f59434722caa70bdc3c9c3806fa
Endzhe Galieva <endzhe91@gmail.com>
2021-05-26 11:11:08 +0300

    use props and state

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -8,7 +8,15 @@ import PostAddForm from '../post-add-form';
 
 import './app.css';
 
+
 const App = () => {
+
+  const data = [
+     {label: 'Going to learn React', important: true, id: 'fgdhb'},
+     {label: 'That is so good', important: false, id: 'gghn'},
+     {label: 'I need a break...', important: false, id: 'gfr'}
+  ];
+
   return (
     <div className="app">
       <AppHeader/>
@@ -16,7 +24,7 @@ const App = () => {
         <SearchPanel/>
         <PostStatusFilter/>
       </div>
-      <PostList/>
+      <PostList posts={data} />
       <PostAddForm/>
     </div>
    )
